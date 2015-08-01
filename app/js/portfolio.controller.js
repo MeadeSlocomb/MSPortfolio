b7e83af8b0b132ad15e739a8f2ab8a27bd059995
Meade Slocomb <meade.slocomb@gmail.com>
2015-08-01 16:09:05 -0400

    Added links to deployed site in the portfolio section.  Added info about popQuiz! app

--- app/js/portfolio.controller.js
+++ app/js/portfolio.controller.js
@@ -11,9 +11,19 @@
       $scope.codebytes = {
         title: 'CodeBytes',
         tagline: 'Code. Conversation. Community.',
+        url: 'http://development.codebytes-deployed.divshot.io/#/',
         image: 'images/codebytes.png',
         desc: 'CodeBytes is a social network for software developers at every stage of the learning process. Users can seek assistance with problems, share "A-ha!" moments, or simply connect with fellow developers.  Posts can include embedded GitHub gists and has a fully sortable/searchable content system.',
         github: 'https://github.com/Code-Bytes'
+      };
+
+      $scope.popquiz = {
+        title: 'popQuiz!',
+        tagline: 'Frivilous trivilous',
+        url: 'http://development.meade-popquiz.divshot.io/#/',
+        image: 'images/popquiz.png',
+        desc: 'popquiz! is a community based trivia app. Players can create quizzes consisting of any number of questions and answers for other players to take.',
+        github: 'https://github.com/MeadeSlocomb/PopQuiz'
       };
 
 
